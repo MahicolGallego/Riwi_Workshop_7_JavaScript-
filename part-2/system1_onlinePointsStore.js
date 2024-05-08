61ec00338a135c44bac003e34c122caf7749258e
Mahi_G13 <maicolgallego14@gmail.com>
2024-05-08 15:44:35 -0500

    WIP: Prototypes of system 1 done

--- part-2/system1_onlinePointsStore.js
+++ part-2/system1_onlinePointsStore.js
@@ -57,7 +57,66 @@ const adminUno = new Administrador("Mahicol", "@123", "pss123", 1234567);
 adminUno.agregarProducto();
 
 //Entidad base/principal Producto -----------------------------------------------------
+
+function Producto(nombre, puntosNecesarios, cantidadDisponible, stock){
+  this.nombre = nombre;
+  this.puntosNecesarios = puntosNecesarios;
+  this.cantidadDisponible = cantidadDisponible;
+  this.stock = stock;
+}
+
+Producto.prototype.obtenerInfo = function(){
+  return `Nombre: ${this.nombre}, puntos necesarios: ${this.puntosNecesarios}, cantidad disponible: ${this.cantidadDisponible}, stock: ${this.stock}`;
+}
+
 //Extension/Herencias de Producto
+
+//Producto fisico
+
+function ProductoFisico(nombre, puntosNecesarios, cantidadDisponible, stock, precio){
+  Producto.call(this, nombre, puntosNecesarios, cantidadDisponible, stock)
+  this.precio = precio;
+
+  this.obtenerInfo = function(){
+    return console.log(`${Producto.prototype.obtenerInfo.call(this)}, Como soy producto fisico tambien tengo un precio: $${this.precio}`)
+  }
+
+  this.actualizarStock = () => {
+    return console.log("Actualizando stock");
+  }
+
+  this.enviarProducto = function(){
+    return console.log("Enviando producto");
+  }
+}
+
+ProductoFisico.prototype = new Producto()
+
+//Ejemplo/Prueba
+
+const productoUno = new ProductoFisico("Camiseta", 100, 10, 100, 20000);
+
+productoUno.obtenerInfo();
+
+//Producto virtual
+
+function ProductoVirtual(nombre, puntosNecesarios, cantidadDisponible, stock, url){
+  Producto.call(this, nombre, puntosNecesarios, cantidadDisponible, stock)
+  this.url = url;
+}
+
+ProductoVirtual.prototype.obtenerInfo = function(){
+  return console.log(`${Producto.prototype.obtenerInfo.call(this)} y Como soy producto virtual tambien tengo una url: $${this.url}`)
+}
+
+ProductoVirtual.prototype.descargar = () => {
+  return console.log("Descargando producto");
+} 
+
+ProductoVirtual.prototype.obtenerProductoEmail = function (){
+  return console.log("Enviando Producto por email");
+}
+
 
 //Entidad Categoria de producto -------------------------------------------------------------
 
@@ -78,5 +137,21 @@ function Actividad(tipo, puntosOtorgados) {
 }
 
 Actividad.prototype.completarActividad = function () {
-  return console.log("Complete actividad");
+  return console.log("Completando actividad");
 };
+
+//Entidad orden de canje  -------------------------------------------------------------
+
+function OrdenCanje(usuario,producto,fecha){
+  this.usuario = usuario;
+  this.producto = producto;
+  this.fecha = fecha;
+
+  this.confirmarOrden = function(){
+    return console.log("Confirmando orden");
+  }
+
+  this.cancelarOrden = () => {
+    return console.log("Cancelando orden");
+  }
+}
